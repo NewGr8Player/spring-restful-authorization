03e15ce92e57e477934225b85660daa7da240b55
NewGr8Player <273221594@qq.com>
2017-10-06 17:34:54 +0800

    修改Swagger-ui配置，删除无用jsp文件

--- src/main/java/com/scienjus/config/SwaggerConfig.java
+++ src/main/java/com/scienjus/config/SwaggerConfig.java
@@ -12,8 +12,9 @@ import java.sql.Timestamp;
 
 /**
  * swagger-ui的配置
+ *
  * @author ScienJus
- * @date 2015/7/10.
+ * @since 2017/10/06
  */
 @Configuration
 @EnableSwagger
@@ -28,10 +29,21 @@ public class SwaggerConfig {
 
     @Bean
     public SwaggerSpringMvcPlugin customImplementation() {
+        /* 对于不想显示的项目直接使用null作为参数即可 */
+        final String title = "基于SpringBoot的RESTful权限验证示例Api";
+        final String description = "案例中包括:<br />无权限验证的/test,<br />有权限验证的/tockens,<br />以及SpringBoot框架自带的/error";
+        final String termsOfServiceUrl = "这里一般是公司主页";
+        final String contact = "273221594@qq.com";
+        final String license = "许可证类型";
+        final String licenseUrl = "许可证链接";
         return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-                .apiInfo(new ApiInfo("Spring RESTful Authorization Demo Api",
-                        null, null, null, null, null)).
-                        //将Timestamp类型全部转为Long类型
+                .apiInfo(new ApiInfo(title,
+                        description,
+                        termsOfServiceUrl,
+                        contact,
+                        license,
+                        licenseUrl)).
+                        /* 将Timestamp类型全部转为Long类型 */
                         directModelSubstitute(Timestamp.class, Long.class);
     }
 
