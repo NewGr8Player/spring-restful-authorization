97e2d1d242ace74e577c619fd0d540da3c42f53f
NewGr8Player <273221594@qq.com>
2017-10-10 12:19:42 +0800

    修正部分注释错误，修正由于IDE导致UserRepository的错误更改。

--- src/main/java/com/scienjus/repository/UserRepository.java
+++ src/main/java/com/scienjus/repository/UserRepository.java
@@ -4,16 +4,16 @@ import com.scienjus.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
- * <p>用户的CRUD基本操作<p/>
+ * <p>用户的CRUD基本操作</p>
  *
  * @date 2017/10/06
  */
-public abstract class UserRepository implements CrudRepository<User, String> {
+public interface UserRepository extends CrudRepository<User, String> {
 
     /**
-     * <p>这个接口并没有明确的实现类，但仍然可以正常使用<p/>
+     * <p>这个接口并没有明确的实现类，但仍然可以正常使用</p>
      *
      * @see <a href="https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details"><b>Spring Data Jpa</b> - 3.4. Defining query methods</a>
      */
-    public abstract User findByUsername(String username);
+    User findByUsername(String username);
 }
