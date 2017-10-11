b73197f2c54c45c273bfde1bc9d0f38ad02d502f
NewGr8Player <273221594@qq.com>
2017-10-11 20:43:33 +0800

    修改错误的描述信息。

--- src/main/java/com/scienjus/config/SwaggerConfig.java
+++ src/main/java/com/scienjus/config/SwaggerConfig.java
@@ -31,7 +31,7 @@ public class SwaggerConfig {
     public SwaggerSpringMvcPlugin customImplementation() {
         /* 对于不想显示的项目直接使用null作为参数即可 */
         final String title = "基于SpringBoot的RESTful权限验证示例Api";
-        final String description = "案例中包括:<br />无权限验证的/test,<br />有权限验证的/tockens,<br />以及SpringBoot框架自带的/error";
+        final String description = "案例中包括:<br />无权限验证的/test,<br />有权限验证的/login,<br />以及SpringBoot框架自带的/error";
         final String termsOfServiceUrl = "这里一般是公司主页";
         final String contact = "273221594@qq.com";
         final String license = "许可证类型";
